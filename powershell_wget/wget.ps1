e388c419670e986c597151edd00331eb9bd5bb1d
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-07-26 12:03:03 -0400

    Create wget.ps1

---
+++ powershell_wget/wget.ps1
@@ -0,0 +1,10 @@
+$url = "http://mirror.internode.on.net/pub/test/10meg.test"
+$output = "$PSScriptRoot\10meg.test"
+$start_time = Get-Date
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+#OR
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
