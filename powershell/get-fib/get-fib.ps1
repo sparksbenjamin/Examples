cae780afa3adbd1bf43ffdd253f8320ddcd31699
Ben <sparksbenjamin@gmail.coim>
2022-03-10 20:09:50 -0500

    updated fib

--- powershell/get-fib/get-fib.ps1
+++ powershell/get-fib/get-fib.ps1
@@ -2,22 +2,42 @@ function Get-fibonacci{
         param(
     [Parameter(Position=0,mandatory=$true)]
     [int]$Requested_possition,
-    $mem=@{}
+    $seed=@{}
     )
     switch ($Requested_possition){
         0 { return 0}
         1 { return 1}
         default { 
-            if ($mem.$Requested_possition){ return $mem.$Requested_possition}
+            if ($seed.$Requested_possition){ return $seed.$Requested_possition}
             else{
+                
                 $n1 = $Requested_possition - 1
                 $n2 = $Requested_possition - 2
-                $n1_value = Get-fibonacci -Requested_possition $n1 -mem $mem
-                $n2_value = Get-fibonacci -Requested_possition $n2 -mem $mem
-                [double]$tvalue = $n1_value + $n2_value
-                $mem.$Requested_possition=$tvalue
+                if($seed.$n1){$n1_value = $seed.$n1}else{
+                    $n1_value = Get-fibonacci -Requested_possition $n1 -seed $seed
+                }
+                if($seed.$n2){$n2_value = $seed.$n2}else{
+                    $n2_value = Get-fibonacci -Requested_possition $n2 -seed $seed
+                }
+                [bigint]$tvalue = $n1_value + $n2_value
+                $seed.$Requested_possition=$tvalue
                 return $tvalue
             }   
         }
     }
+}
+function Get-Fib-Seq{
+    param(
+        [int]$Starting_Num,
+        [int]$Count
+    )
+    $output=@{}
+    $max = $Starting_Num + $Count
+    for($i=$Starting_Num; $i -le $max;$i++){
+        $value = Get-fibonacci -Requested_possition $i
+        $output.$i = $value
+    }
+    return $output.GetEnumerator() | sort -Property Name
+    $i=0
+    $output=''
 }
