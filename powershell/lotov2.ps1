bb43758774032d49effe8dba56057eb01a39c52e
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2022-03-31 19:44:20 -0400

    Update lotov2.ps1

--- powershell/lotov2.ps1
+++ powershell/lotov2.ps1
@@ -41,6 +41,7 @@ function Get-Pick{
     param(
         [int]$Number,
         $WeightedData=@{},
+        $data=@{},
         [int]$points,
         [bool]$Powerball,
         $badNum=@()
@@ -51,32 +52,47 @@ function Get-Pick{
         $num = Get-Lotto-Number
     }
     if ($badNum -contains $num){
-        Get-Pick -badNum $badNum -WeightedData $WeightedData -Powerball $Powerball -points $points
+        Get-Pick -badNum $badNum -WeightedData $WeightedData -Powerball $Powerball -points $points -data $data
     }
     $bouncePick = $false
     #Write-Debug $data
     $w = $WeightedData
     $c = $w | Where-Object -Property Name -Value $num -EQ
     $c = $c.Count
-    $d_avg = $WeightedData.Count | Measure-Object -Average | Select-Object Average
-    $d_avg = [math]::Round($d_avg.Average)
-    $score = [math]::Round($data[$num].count/$points)*100
+    $wc = 0
+    $WeightedData | Select-Object Count | ForEach-Object {
+        $wc += $_.Count
+    }
+    $d_avg = $wc / $points
+    $score = ($c/$points)*100
+    $score = 100 - $score
     #$score = 100 - $score
+    <#
+    Write-host "++++[View]+++"
+    Write-Host "Number: $num"
+    Write-Host "Count: $c"
+    Write-Host "Score: $score"
+    Write-Host "  Avg: $d_avg"
+    Write-host "+++++++++++++"
+    
     Write-Host "$num with count of $c vs an average of $d_avg score: $score"
+    #>
+    if($score -lt 90){ $bouncePick = $true}
     if($IsPowerball){
 
     }else{
-        if($c -gt $d_avg){ $bouncePick = $true}
+        
     }
     if($bouncePick){
         $badNum += $num
-        Get-Pick -badNum $badNum -WeightedData $WeightedData -Powerball $Powerball -points $points
+        Get-Pick -badNum $badNum -WeightedData $WeightedData -Powerball $Powerball -points $points -data $data
     }else{
         return $num
     }
     
     #Write-Host $data
 }
+
 function Get-lottery-numbers{
     param(
         $badSeq=@(),
@@ -85,7 +101,7 @@ function Get-lottery-numbers{
     $bounceSeq = $false
     #$data = Get-Lotto-data
     $points = $data.Count
-    Write-Host $points
+    #Write-Host $points
     $avg_val = $data.DVal | Measure-Object -Average 
     #Write-Host $avg_val
     $avg_val = [math]::Round($avg_val.Average)
@@ -94,7 +110,7 @@ function Get-lottery-numbers{
     $avg_val_max = $avg_val + 42
     $picks = @{}
     foreach($i in 1..6){
-        Write-Host "Working on Pick $i"
+        #Write-Host "Working on Pick $i"
         $weight = @{}
         $pball = $false
         switch ($i) {
@@ -132,6 +148,10 @@ function Get-lottery-numbers{
         #Write-Host $picks
     }
     $i = 0
+    $upicks = $picks | Select-Object -Property Name,Values -Unique
+    if($upicks.length -ne $picks.length){
+        $bounceSeq = $true
+    }
     $p1 = $picks[1]
     $p2 = $picks[2]
     $p3 = $picks[3]
@@ -141,13 +161,13 @@ function Get-lottery-numbers{
     $pv = $p1 + $p2 + $p3 + $p4 + $p5 + $p6
     $pv = $pv
     $seq = "$p1,$p2,$p3,$p4,$p5,$p6"
-    Write-Host $seq
+    #Write-Host $seq
     if($badSeq -contains $seq){$bounceSeq = $true}
     if($data.field_winning_numbers -contains $seq){
         Write-Host "Skipping $seq as it has already won!"
         $bounceSeq = $true      
     }elseif($pv -in $avg_val_min..$avg_val_max){
-        return $picks
+        return $seq
     }else{
         $bounceSeq = $true
     }
@@ -157,9 +177,52 @@ function Get-lottery-numbers{
     }
     #Write-Host $seq
 }
+function Get-Seq-Val{
+    param(
+        $seq
+    )
+    $array = $seq.Split(",")
+    $total=0
+    foreach($i in $array){
+        $total += $i
+    }
+    return $total
+}
+function Get-Seq-Score{
+    param(
+        [string]$seq,
+        $data
+    )
+    $score = 100
+    $s_val = Get-Seq-Val -seq $seq
+    $avg_ticket_value = $data.DVal | Measure-Object -Average | Select-Object Average
+    $avg_ticket_value = [math]::Round($avg_ticket_value.Average)
+    if($s_val -ge $avg_ticket_value){
+        $val_score = $s_val - $avg_ticket_value
+    }else{
+        $val_score = $avg_ticket_value - $s_val
+    }
+    #write-host $val_score
+    $score = $score - $val_score
+    return $score
+}
 $data_file_path = "C:\Users\Spark\Downloads\results.csv"
 #Get-Lotto-data -Path $data_file_path
 $data = Import-Csv -Path $data_file_path
-Get-lottery-numbers -data $data
-#Get-Ran-seq -ravg $avg
+
+#Get-Average -array $wc
+#Get-Average -array $weight.Count
+$winners=@()
+$i = 1
+foreach($i in 1..1000){
+    $score = 0
+    $seq = Get-lottery-numbers -data $data -badSeq $winners
+    $score = Get-Seq-Score -seq $seq -data $data
+    $ticket = 
+    @([PSCustomObject]@{SEQ=$seq;SCORE=$score})
+    $winners += $ticket   
+   
+}
+#$seq, $score
+$winners 
 
